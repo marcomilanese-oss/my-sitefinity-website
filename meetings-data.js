145bade9e0cec6fa79c06682e107e002ae9cf55c
GFCM Automation <devops@gfcm.org>
2026-06-10 09:25:22 +0000

    Make meetings data export generic

--- meetings-data.js
+++ meetings-data.js
@@ -1,123 +1,620 @@
+window.GFCM_MEETINGS_COLUMNS = [
+  {
+    "key": "createdOn",
+    "label": "Created On",
+    "type": "date"
+  },
+  {
+    "key": "createdBy",
+    "label": "Created By",
+    "type": "text"
+  },
+  {
+    "key": "createdByDelegate",
+    "label": "Created By Delegate",
+    "type": "text"
+  },
+  {
+    "key": "modifiedOn",
+    "label": "Modified On",
+    "type": "date"
+  },
+  {
+    "key": "modifiedBy",
+    "label": "Modified By",
+    "type": "text"
+  },
+  {
+    "key": "modifiedByDelegate",
+    "label": "Modified By Delegate",
+    "type": "text"
+  },
+  {
+    "key": "importSequenceNumber",
+    "label": "Import Sequence Number",
+    "type": "number"
+  },
+  {
+    "key": "recordCreatedOn",
+    "label": "Record Created On",
+    "type": "date"
+  },
+  {
+    "key": "venue",
+    "label": "Venue",
+    "type": "text"
+  },
+  {
+    "key": "websiteSection",
+    "label": "Website Section",
+    "type": "lookup"
+  },
+  {
+    "key": "websiteType",
+    "label": "Website Type",
+    "type": "lookup"
+  },
+  {
+    "key": "websiteMeetings",
+    "label": "Website Meetings",
+    "type": "text"
+  },
+  {
+    "key": "appearsInWebsite",
+    "label": "Appears in Website Y/N",
+    "type": "boolean"
+  },
+  {
+    "key": "city",
+    "label": "City",
+    "type": "text"
+  },
+  {
+    "key": "country",
+    "label": "Country",
+    "type": "text"
+  },
+  {
+    "key": "expertGroup",
+    "label": "Expert Group",
+    "type": "text"
+  },
+  {
+    "key": "meetingCategory",
+    "label": "Meeting Category",
+    "type": "text"
+  },
+  {
+    "key": "meetingCode",
+    "label": "Meeting Code",
+    "type": "text"
+  },
+  {
+    "key": "meetingTitle",
+    "label": "Meeting Title",
+    "type": "text"
+  },
+  {
+    "key": "meetingType",
+    "label": "Meeting Type",
+    "type": "text"
+  },
+  {
+    "key": "session",
+    "label": "Session",
+    "type": "text"
+  },
+  {
+    "key": "meetingStartDate",
+    "label": "Meeting Start Date",
+    "type": "date"
+  },
+  {
+    "key": "meetingEndDate",
+    "label": "Meeting End Date",
+    "type": "date"
+  },
+  {
+    "key": "pkWebsiteMeetingEntry",
+    "label": "PK_Website_Meeting_Entry",
+    "type": "text"
+  },
+  {
+    "key": "publicWebsiteUrl",
+    "label": "Public website URL",
+    "type": "url"
+  },
+  {
+    "key": "publicReportUrlEn",
+    "label": "Public Report URL (EN)",
+    "type": "url"
+  },
+  {
+    "key": "publicReportUrlFr",
+    "label": "Public Report URL (FR)",
+    "type": "url"
+  },
+  {
+    "key": "publicReportUrlAr",
+    "label": "Public Report URL (AR)",
+    "type": "url"
+  },
+  {
+    "key": "publicReportUrlSp",
+    "label": "Public Report URL (SP)",
+    "type": "url"
+  },
+  {
+    "key": "websiteLinkStayClickableUponExpiration",
+    "label": "Website link stay clickable upon expiration",
+    "type": "boolean"
+  },
+  {
+    "key": "status",
+    "label": "Status",
+    "type": "text"
+  },
+  {
+    "key": "statusReason",
+    "label": "Status Reason",
+    "type": "text"
+  },
+  {
+    "key": "owner",
+    "label": "Owner",
+    "type": "lookup"
+  },
+  {
+    "key": "owningBusinessUnit",
+    "label": "Owning Business Unit",
+    "type": "lookup"
+  },
+  {
+    "key": "owningTeam",
+    "label": "Owning Team",
+    "type": "lookup"
+  },
+  {
+    "key": "owningUser",
+    "label": "Owning User",
+    "type": "lookup"
+  },
+  {
+    "key": "timeZoneRuleVersionNumber",
+    "label": "Time Zone Rule Version Number",
+    "type": "number"
+  },
+  {
+    "key": "utcConversionTimeZoneCode",
+    "label": "UTC Conversion Time Zone Code",
+    "type": "number"
+  },
+  {
+    "key": "versionNumber",
+    "label": "Version Number",
+    "type": "number"
+  }
+]
+;
+
+window.GFCM_MEETINGS_METADATA = {
+  "generatedAt": "2026-06-10T09:25:00.2445657Z",
+  "source": "Dataverse",
+  "environment": "GFCM Extranet Environment",
+  "table": "new_website_meetingses",
+  "tableDisplayName": "Website_Meetings",
+  "tableId": "gfcm-website-meetings"
+}
+;
+
 window.GFCM_MEETINGS_DATA = [
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rome",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:42",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:42",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "FAO HQ",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rome",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132388",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rome",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rome",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:12",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:36",
+    "modifiedBy": "Chiara Baffoni",
+    "modifiedByDelegate": "Chiara Baffoni",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "FAO HQ",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rome",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132360",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rome",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Batumi",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:32",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:32",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "NEA",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Batumi",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132327",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Batumi",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rome",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:29",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:29",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "FAO HQ",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rome",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132308",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rome",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rome",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:22",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:22",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "FAO HQ",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rome",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132227",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rome",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rhodes",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:19",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:19",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "-",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rhodes",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44132212",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rhodes",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Trabzon",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "08/06/2026 10:15",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 10:15",
+    "modifiedBy": "Christina Raftoudi",
+    "modifiedByDelegate": "",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "SUMAE - Central Fisheries Research Institute",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Trabzon",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44131756",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Trabzon",
+      "date": "",
+      "reports": []
+    }
   },
   {
-    "year": 2026,
-    "title": "Untitled meeting",
-    "url": "#",
-    "type": "Meeting",
-    "category": "",
-    "location": "Rome",
-    "date": "",
-    "reports": [
-      {
-        "label": "EN",
-        "url": "#"
-      }
-    ]
+    "createdOn": "04/06/2026 11:45",
+    "createdBy": "Christina Raftoudi",
+    "createdByDelegate": "",
+    "modifiedOn": "08/06/2026 09:53",
+    "modifiedBy": "Chiara Baffoni",
+    "modifiedByDelegate": "Chiara Baffoni",
+    "importSequenceNumber": "",
+    "recordCreatedOn": "",
+    "venue": "FAO HQ",
+    "websiteSection": "",
+    "websiteType": "",
+    "websiteMeetings": "",
+    "appearsInWebsite": "",
+    "city": "Rome",
+    "country": "",
+    "expertGroup": "",
+    "meetingCategory": "",
+    "meetingCode": "",
+    "meetingTitle": "",
+    "meetingType": "",
+    "session": "",
+    "meetingStartDate": "",
+    "meetingEndDate": "",
+    "pkWebsiteMeetingEntry": "",
+    "publicWebsiteUrl": "",
+    "publicReportUrlEn": "",
+    "publicReportUrlFr": "",
+    "publicReportUrlAr": "",
+    "publicReportUrlSp": "",
+    "websiteLinkStayClickableUponExpiration": "",
+    "status": "Active",
+    "statusReason": "Active",
+    "owner": "Christina Raftoudi",
+    "owningBusinessUnit": "org02cf0e5c",
+    "owningTeam": "",
+    "owningUser": "",
+    "timeZoneRuleVersionNumber": "0",
+    "utcConversionTimeZoneCode": "",
+    "versionNumber": "44131637",
+    "_display": {
+      "year": 2026,
+      "title": "Untitled meeting",
+      "url": "#",
+      "type": "Meeting",
+      "category": "",
+      "location": "Rome",
+      "date": "",
+      "reports": []
+    }
   }
 ]
 ;
