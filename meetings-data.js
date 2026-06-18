0751a2568cb163fbf3db403f96fbd0cc93862722
GFCM Automation <devops@gfcm.org>
2026-06-18 22:30:20 +0000

    Make meetings data export fully generic

--- meetings-data.js
+++ meetings-data.js
@@ -1,6 +1,6 @@
 window.GFCM_MEETINGS_EXPORT = {
   "metadata": {
-    "generatedAt": "2026-06-18T22:15:00.9980344Z",
+    "generatedAt": "2026-06-18T22:30:01.2857404Z",
     "source": "Dataverse",
     "environment": "GFCM Extranet Environment",
     "table": "new_website_meetingses",
