708a3a6131e9c579c953b4eb25efe72eefb12fc0
GFCM Automation <devops@gfcm.org>
2026-06-23 14:40:04 +0000

    Update TableContents.js from Power Automate data

--- TableContents.js
+++ TableContents.js
@@ -1,57 +1,43 @@
 window.myTableData = [
   {
-    "year": 2024,
-    "type": "Res",
-    "category": "CM",
-    "number": 1,
-    "title": "Monitoring framework",
-    "files": [
-      { "label": "EN", "url": "#" },
-      { "label": "FR", "url": "#" }
-    ]
+    "title": "OTH-GFCM/30/2006/1",
+    "modified": ""
   },
   {
-    "year": 2024,
-    "type": "Rec",
-    "category": "SAC",
-    "number": 2,
-    "title": "Marine ecosystems",
-    "files": [
-      { "label": "EN", "url": "#" },
-      { "label": "FR", "url": "#" }
-    ]
+    "title": "REC-CM-GFCM/13/1976/3",
+    "modified": ""
   },
   {
-    "year": 2023,
-    "type": "Res",
-    "category": "CAQ",
-    "number": 3,
-    "title": "Aquaculture innovation",
-    "files": [
-      { "label": "EN", "url": "#" },
-      { "label": "FR", "url": "#" }
-    ]
+    "title": "REC.MCS-GFCM/22/1997/1",
+    "modified": ""
   },
   {
-    "year": 2023,
-    "type": "Dec",
-    "category": "BFT",
-    "number": 4,
-    "title": "Tuna measures",
-    "files": [
-      { "label": "EN", "url": "#" },
-      { "label": "FR", "url": "#" }
-    ]
+    "title": "REC.CM-GFCM/27/2002/1",
+    "modified": ""
   },
   {
-    "year": 2022,
-    "type": "Rec",
-    "category": "CM",
-    "number": 5,
-    "title": "Port inspections",
-    "files": [
-      { "label": "EN", "url": "#" },
-      { "label": "FR", "url": "#" }
-    ]
+    "title": "REC.CM-GFCM/29/2005/1",
+    "modified": ""
+  },
+  {
+    "title": "REC.CM-GFCM/30/2006/1",
+    "modified": ""
+  },
+  {
+    "title": "REC.CM-GFCM/30/2006/2",
+    "modified": ""
+  },
+  {
+    "title": "REC.CM-GFCM/30/2006/3",
+    "modified": ""
+  },
+  {
+    "title": "REC.CM-GFCM/31/2007/1",
+    "modified": ""
+  },
+  {
+    "title": "REC.CM-GFCM/33/2009/1",
+    "modified": ""
   }
 ]
+;
