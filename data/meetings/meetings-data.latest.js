a11ea9422aea325baf2ce7405780b448d82d127e
GFCM Automation <devops@gfcm.org>
2026-06-29 13:00:43 +0000

    Update versioned meetings data export

--- data/meetings/meetings-data.latest.js
+++ data/meetings/meetings-data.latest.js
@@ -1,6 +1,6 @@
 window.GFCM_MEETINGS_LATEST = {
-  "file": "meetings-data-20260629T124527Z.js",
-  "path": "data/meetings/meetings-data-20260629T124527Z.js",
-  "generatedAt": "2026-06-29T12:45:27Z",
+  "file": "meetings-data-20260629T130039Z.js",
+  "path": "data/meetings/meetings-data-20260629T130039Z.js",
+  "generatedAt": "2026-06-29T13:00:39Z",
   "source": "Dataverse"
 };
