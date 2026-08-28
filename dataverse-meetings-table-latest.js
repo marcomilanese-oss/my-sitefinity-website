e25490e2ce071c20d3cd2c32f5fc14d63aca8eb4
GFCM Automation <devops@gfcm.org>
2026-08-28 16:00:36 +0000

    Update versioned dataverse meetings table export

--- dataverse-meetings-table-latest.js
+++ dataverse-meetings-table-latest.js
@@ -1,2 +1,2 @@
-window.GFCM_MEETINGS_LATEST = {"file":"dataverse-meetings-table-20260828-1401.js","path":"dataverse-meetings-table-20260828-1401.js","generatedAt":"2026-08-28T14:01:25Z","source":"Dataverse"}
+window.GFCM_MEETINGS_LATEST = {"file":"dataverse-meetings-table-20260828-1600.js","path":"dataverse-meetings-table-20260828-1600.js","generatedAt":"2026-08-28T16:00:30Z","source":"Dataverse"}
 ;
